8917a7e9fcb0d54d266944a02c03654a45dbe62e
Sicelo Mabasa <sl.mabasa98@gmail.com>
2025-03-19 23:23:38 +0200

    Update wsgi.py
    allow render

--- storefront/wsgi.py
+++ storefront/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from app import app
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 400))
+  app.run(host="0.0.0.0", port=port)
