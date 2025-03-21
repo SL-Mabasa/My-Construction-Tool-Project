f17ee504eaa99c1de1281e9867a9612c1c3e0ec0
Sicelo Mabasa <sl.mabasa98@gmail.com>
2025-03-21 12:36:52 +0200

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import action, customers, product, category, order
+#from .models import action, customers, product, category, order
 
 # Register your models here.
 
