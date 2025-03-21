85ce61cfd50a9c0393e3039d839fc648ed571681
Sicelo Mabasa <sl.mabasa98@gmail.com>
2025-03-21 12:25:01 +0200

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,8 @@ from .models import action, customers, product, category, order
 
 # Register your models here.
 
-admin.site.register(action)
+'''admin.site.register(action)
 admin.site.register(customers)
 admin.site.register(product)
 admin.site.register(category)
-admin.site.register(order)
+admin.site.register(order)'''
