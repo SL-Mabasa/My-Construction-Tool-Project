3c0f796b9e44bfeeb77a8704cf58e30ebe3aa0b8
Sicelo Mabasa <sl.mabasa98@gmail.com>
2025-03-22 03:14:28 +0200

    Update urls.py

--- storefront/urls.py
+++ storefront/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 #hello
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls'))
+    path('main/', include('main.urls')),
+    path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
